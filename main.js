1e9caa3e54aee3932fe0cfa36d446a3e0e5ab03f
Dshane Godbold <dshanegodbold@gmail.com>
2022-02-08 11:29:51 -0600

    update

--- main.js
+++ main.js
@@ -1,44 +1,19 @@
 import input from "input";
 
-function say(num) {
-  switch (num) {
-    case "1":
-      console.log("One");
-      break;
-    case "2":
-      console.log("Two");
-      break;
-    case "3":
-      console.log("Three");
-      break;
-    case "4":
-      console.log("Four");
-      break;
-    case "5":
-      console.log("Five");
-      break;
-    case "6":
-      console.log("Six");
-      break;
-    case "7":
-      console.log("Seven");
-      break;
-    case "8":
-      console.log("Eight");
-      break;
-    case "9":
-      console.log("Nine");
-      break;
-    case "10":
-      console.log("Ten");
-      break;
-  }
-}
-async function main() {
-  for (let i = 1; i <= 10; i += 1) {
-    let iStr = i.toString();
-    say(iStr);
+function square(size) {
+  for (let i = 1; i <= size; i += 1) {
+    for (let hght = 1; hght <= 1; hght += 1) {
+      let output = "";
+      for (let len = 0; len < size; len += 1) {
+        output += 'X';
+      }
+      console.log(output);
+    }
   }
 }
 
+async function main() {
+  const size = await input.text("enter a size");
+  square(size);
+}
 main();
