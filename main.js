8fd0af3f2e46d14ceb1429270e3cfb8ed0bb5602
Dshane Godbold <dshanegodbold@gmail.com>
2022-02-08 13:54:55 -0600

    shoppinh

--- main.js
+++ main.js
@@ -1,15 +1,8 @@
-const shopping = [
-    'Ribeye',
-    'Orange',
-    'Milk',
-    'Water',
-    'Kiwi',
-    'Bread',
-];
+const shopping = ["Ribeye", "Orange", "Milk", "Water", "Kiwi", "Bread"];
 
-async function main(){
-    shopping.forEach(element =>console.log(element))
-    
-    
+async function main() {
+  console.log("Here is your shopping list");
+  console.log("===========================");
+  shopping.forEach((element) => console.log(element));
 }
-main()
+main();
