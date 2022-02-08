334f68a6dd2c558c6d25955d38ade9e8f366b75e
Dshane Godbold <dshanegodbold@gmail.com>
2022-02-08 13:28:08 -0600

    recursion

--- main.js
+++ main.js
@@ -1,19 +1,10 @@
-import input from "input";
-
-function square(size) {
-  for (let i = 1; i <= size; i += 1) {
-    for (let hght = 1; hght <= 1; hght += 1) {
-      let output = "";
-      for (let len = 0; len < size; len += 1) {
-        output += 'X';
-      }
-      console.log(output);
-    }
-  }
+function recursion(i) {
+           console.log(i);
+        i += 1;
+        recursion(i)
+}
+async function main (){
+recursion(0)
 }
 
-async function main() {
-  const size = await input.text("enter a size");
-  square(size);
-}
-main();
+main()
